e8d389ce413686593954d898d0f826b302b88927
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-11-06 20:34:03 +0530

    DSA - Patterns

---
+++ Lecture4_Patterns.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+using namespace std;
+
+//          In this lecture we will basically solve so many patterns...                   
+
+int main(){
+
+    // Pattern 1.
+
+    int n;
+    cout<<"Enter the number of row and columns you want: ";
+
+    cin>>n;
+
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int j = 1;j<=n;j++){
+    //         cout<<j;
+    //     }
+
+    //     cout<<"\n";
+
+    // }
+
+    // Pattern 2.
+
+    // int  count = 1 ;
+
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int j = 1;j<=i;j++){    
+    //         cout<<count;
+    //         count++;
+    //     }
+
+    //     cout<<"\n";
+
+    // }
+
+    // Pattern 3.
+
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int j = 1;j<=i;j++){    
+    //         cout<<i-j+1;
+    //     }
+
+    //     cout<<"\n";
+
+    // }   
+    // Pattern 4.
+    
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int j = 1;j<=n;j++){    
+    //         char c = 'A'+i-1;
+    //         cout<<c;
+    //     }
+
+    //     cout<<"\n";
+
+    // }
+
+    // Pattern 5.
+
+    // char c = 'A';
+
+    // for(int i = 1;i<=n;i++){
+        
+    //     for(int j = 1;j<=n;j++){  
+
+    //         cout<<c;
+    //         c++;
+    //     }
+
+    //     cout<<"\n";
+    // }
+
+    // Pattern 6.
+
+    // char c = 'A';  
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){  
+    //         cout<<c;
+    //         c++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // Pattern 7.
+
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int j = 1;j<=i;j++){  
+    //         char c = 'A'+i-j;  
+    //         cout<<c;
+    //         c++;
+    //     }
+
+    //     cout<<"\n";
+
+    // }
+    
+    // Pattern 8.
+
+    // for(int i = 1;i<=n;i++){
+
+    //     char c = ('A'+n-i);
+
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<c;
+    //         c++;
+    //     }
+
+    //     cout<<"\n";
+    // }
+
+    // Pattern 9.
+
+    // for(int i = 1;i<=n;i++){
+
+    //     for(int k = 1; k<=n-i;k++){
+    //         cout<<" ";
+    //     }
+
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<"*";
+    //     }
+
+    //     cout<<"\n";
+    // }
+    
+    // Pattern 10.
+
+    // for(int i = 0;i<=n;i++){
+
+    //     for(int j = 1;j<=n-i;j++){
+
+    //         cout<<"*";
+
+    //     }
+
+    //     cout<<"\n";
+    // }
+
+
+    // Pattern 11.
+ 
+
+}
